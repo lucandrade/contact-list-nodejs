d4c9f13a0d61aa26cc7b39f1b97d4e6d4bef53df
Lucas Andrade <lucas.andrade.oliveira@hotmail.com>
2016-12-05 20:30:31 -0200

    adding cors support

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
+import cors from 'cors';
 import Users from './users';
 import Contacts from './contacts';
 
 export default app => {
+    if (process.env.NODE_ENV === 'development') {
+        app.use(cors({
+            origin: true,
+            credentials: true,
+        }));
+    }
+
     Users(app);
     Contacts(app);
 };
